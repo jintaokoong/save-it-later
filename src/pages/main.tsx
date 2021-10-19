dc59f417e2e71a029f7d41e95798886a37ecc056
Jintao Koong <jintaokoong99@gmail.com>
2021-10-19 17:28:50 +0800

    feat: add navbar

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,5 +1,5 @@
 import { auth } from 'config/firebase';
-import { Route, Switch } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
 import ArticlePage from 'pages/article';
 import ArticleDetailPage from 'pages/article-detail';
 import { Fragment } from 'react';
@@ -7,12 +7,26 @@ import { Fragment } from 'react';
 const MainPage = () => {
   return (
     <Fragment>
-      <div>
-        <button type={'button'} onClick={() => auth.signOut()}>
-          logout
-        </button>
-      </div>
-      <div className={'container mx-auto'}>
+      <nav className={'container mx-auto flex justify-between p-3 border-b-2'}>
+        <div className={'branding font-bold inline-flex self-center'}>
+          <Link to={'/'}>Save It Later</Link>
+        </div>
+        <div className={'flex items-center'}>
+          <div className={'hidden sm:block text-blue-900 mr-2'}>
+            {auth.currentUser?.email}
+          </div>
+          <button
+            className={
+              'p-1 px-3 transition duration-500 ease-in-out bg-red-300 disabled:bg-red-50 disabled:cursor-not-allowed disabled:text-red-500 text-red-900 rounded hover:bg-red-200 active:bg-red-300'
+            }
+            type={'button'}
+            onClick={() => auth.signOut()}
+          >
+            Logout
+          </button>
+        </div>
+      </nav>
+      <div className={'container mx-auto p-3'}>
         <Switch>
           <Route exact path={'/main/article'} component={ArticlePage} />
           <Route
